14bf20dde756359b418d53d871d90940636aafa4
ivan-shork <494476749@qq.com>
2021-08-09 08:20:02 +0800

    隐式意图使用

--- app/src/main/java/com/aven/myfirstandroidapplication/ActivityTwo.java
+++ app/src/main/java/com/aven/myfirstandroidapplication/ActivityTwo.java
@@ -3,6 +3,7 @@ package com.aven.myfirstandroidapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,17 +18,31 @@ public class ActivityTwo extends AppCompatActivity implements View.OnClickListen
 
         Button btn1 = (Button) findViewById(R.id.button7);
         btn1.setOnClickListener(ActivityTwo.this);
+        Button baiduBtn = (Button) findViewById(R.id.baidu);
+        baiduBtn.setOnClickListener(ActivityTwo.this);
+        Button phoneBtn = (Button) findViewById(R.id.baidu);
+        phoneBtn.setOnClickListener(ActivityTwo.this);
     }
 
     @Override
     public void onClick(View view) {
-        int btn7 = R.id.button7;
-        if(view.getId() == btn7) {
-            Toast.makeText(ActivityTwo.this,"跳转回首页", Toast.LENGTH_SHORT).show();
-            Intent i = new Intent(ActivityTwo.this, ActivityOne.class);
-            startActivity(i);
-            System.out.print("跳转首页成功！！！！！！！");
+        switch (view.getId()) {
+            case R.id.button7:
+                Toast.makeText(ActivityTwo.this,"跳转回首页", Toast.LENGTH_SHORT).show();
+                Intent i = new Intent(ActivityTwo.this, ActivityOne.class);
+                startActivity(i);
+                System.out.print("跳转首页成功！！！！！！！");
+                break;
+            case R.id.baidu:
+                Intent baiduIndent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.baidu.com"));
+                startActivity(baiduIndent);
+                break;
+            case R.id.phone:
+                Intent phoneIndent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:15815200256"));
+                startActivity(phoneIndent);
+                break;
+            default:
+                break;
         }
-
     }
 }
